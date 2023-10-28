3753f31660f466c61835833ba9c3546cae3ebd71
KenielDev <kenielalves3@gmail.com>
2023-10-28 11:44:00 -0300

    alterado db

--- prisma/migrations/20231022142333_att/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `categoria` DROP FOREIGN KEY `categoria_requestItemId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `variacao` DROP FOREIGN KEY `variacao_requestItemId_fkey`;
